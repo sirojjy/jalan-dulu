58f6a10e9abd7ae064c77449fec3eac880fb12fe
siroj-pc <siroj.bi@gmail.om>
2024-08-07 08:20:54 +0700

    Dashboard 5

--- pages/dashboard/index_dashboard.js
+++ pages/dashboard/index_dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../../components/layout/Layout';
+import SidebarDashboard from './sidebar_d';
 
 const IndexDashboard = () => {
 
@@ -8,27 +9,7 @@ const IndexDashboard = () => {
       <Layout>
         <div className="flex min-h-screen">
           {/* Sidebar */}
-          <div className="flex flex-col w-64 text-white bg-gray-800">
-            <div className="flex items-center justify-center h-16 bg-gray-900">
-              <h1 className="text-2xl font-bold">My Dashboard</h1>
-            </div>
-            <nav className="flex-1 px-4 py-8">
-              <ul>
-                <li className="mb-2">
-                  <a href="#" className="block px-4 py-2 rounded hover:bg-gray-700">Home</a>
-                </li>
-                <li className="mb-2">
-                  <a href="#" className="block px-4 py-2 rounded hover:bg-gray-700">Analytics</a>
-                </li>
-                <li className="mb-2">
-                  <a href="#" className="block px-4 py-2 rounded hover:bg-gray-700">Sales</a>
-                </li>
-                <li className="mb-2">
-                  <a href="#" className="block px-4 py-2 rounded hover:bg-gray-700">Messages</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
+          <SidebarDashboard></SidebarDashboard>
 
           {/* Main Content */}
           <div className="flex-1 p-8 bg-gray-100">
